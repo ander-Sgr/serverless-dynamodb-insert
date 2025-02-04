ce76c99f5877fc4354ac998c5d164fac08f6b9d3
ander <andersonbarreto2001@gmail.com>
2025-02-04 08:13:51 +0100

    to do

--- serverless/terraform/main.tf
+++ serverless/terraform/main.tf
@@ -2,10 +2,18 @@
 resource "aws_dynamodb_table" "users_table" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "userid"
+  hash_key     = var.hash_key
 
   attribute {
-    name = "userid"
+    name = var.hash_key
     type = "S"
   }
+}
+
+resource "aws_lambda_function" "insert_handler" {
+
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+
 }
